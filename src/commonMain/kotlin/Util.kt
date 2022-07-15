93d1669c363e0d1499904a98a6a1182d489cf5f0
Tom Yuval <tomuvak@gmail.com>
2022-07-15 23:30:22 +0200

    Util: add mootPredicate

--- src/commonMain/kotlin/Util.kt
+++ src/commonMain/kotlin/Util.kt
@@ -1,3 +1,10 @@
 package com.tomuvak.testing.assertions
 
+import kotlin.test.DefaultAsserter.fail
+
+/**
+ * A predicate which asserts it's never actually called.
+ */
+val mootPredicate: (Any?) -> Boolean = { fail("Not supposed to actually be called") }
+
 fun messagePrefix(message: String?): String = if (message == null) "" else "$message. "
--- src/commonTest/kotlin/UtilTest.kt
+++ src/commonTest/kotlin/UtilTest.kt
@@ -4,6 +4,10 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class UtilTest {
+    @Test fun mootPredicate() {
+        assertFailsWithTypeAndMessageContaining<AssertionError>("ot supposed", "be called") { mootPredicate(Unit) }
+    }
+
     @Test fun emptyMessagePrefix() = assertEquals("", messagePrefix(null))
     @Test fun nonEmptyMessagePrefix() = assertEquals("Test. ", messagePrefix("Test"))
 }
