e84677426f8a8181ff32d944557da3e9cfc04b13
Tom Yuval <tomuvak@gmail.com>
2022-07-16 21:51:01 +0200

    Functions: add MockFunction

--- src/commonMain/kotlin/Functions.kt
+++ src/commonMain/kotlin/Functions.kt
@@ -6,3 +6,17 @@ import kotlin.test.fail
  * A function which asserts it's never actually called.
  */
 val mootFunction: (Any?) -> Nothing = { fail("Not supposed to actually be called") }
+
+/**
+ * Instances of this class can serve as functions from [TInput] to [TOutput] (either directly, or, when needed, through
+ * their [invoke] function). The actual function which gets executed and whose return value is returned is [function].
+ * Each invocation adds the argument passed to it to [calls].
+ */
+class MockFunction<TInput, TOutput>(var function: (TInput) -> TOutput) {
+    val calls: MutableList<TInput> = mutableListOf()
+
+    operator fun invoke(argument: TInput): TOutput {
+        calls.add(argument)
+        return function(argument)
+    }
+}
--- src/commonTest/kotlin/FunctionsTest.kt
+++ src/commonTest/kotlin/FunctionsTest.kt
@@ -1,8 +1,34 @@
 package com.tomuvak.testing.assertions
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class FunctionsTest {
     @Test fun mootFunction() =
         assertFailsWithTypeAndMessageContaining<AssertionError>("ot supposed", "be called") { mootFunction(Unit) }
+
+    @Test fun mockFunction() {
+        val calls = mutableListOf<Int>()
+        var mockResult = ""
+        val mock = MockFunction<Int, String> {
+            calls.add(it)
+            mockResult
+        }
+        fun verify(argument: Int, result: String) {
+            calls.clear()
+            mockResult = result
+            val originalRecordedCalls = mock.calls.toList()
+
+            assertEquals(result, mock(argument))
+
+            assertEquals(listOf(argument), calls)
+            assertEquals(originalRecordedCalls + argument, mock.calls)
+        }
+
+        assertEquals(emptyList(), mock.calls)
+        verify(3, "abc")
+        verify(-7, "asdfg")
+        verify(20, "")
+        verify(0, "xyz")
+    }
 }
